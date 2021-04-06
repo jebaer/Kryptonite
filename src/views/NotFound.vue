9827b53fb6c1c2a91a10729cbdd1cc99a67da4c4
jebaer <jrmybrcf@gmail.com>
2021-04-06 10:06:36 +0200

    update notfound view

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col justify-center w-full items-center">
         <h1 class="text-2xl text-secondary-base font-bold">OUPS</h1>
-        <h2 class="text-xl text-main-base">PAGE NOT FOUND</h2>
+        <h2 class="text-xl text-main-base font-semibold">PAGE NOT FOUND</h2>
     </div>
 </template>
 
